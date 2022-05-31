1e6c57e2fa6a0e63913b55f13fac2c335780c789
fDll37 <dmendelev@yandex.ru>
2022-05-31 23:54:17 +0300

    refactoring (ver 1.2)

--- Right on target/Model/Game.swift
+++ Right on target/Model/Game.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+
+protocol GameProtocol {    // Количество заработанных очков
+    var score: Int { get }    // Загаданное значение
+    var currentSecretValue: Int { get }    // Проверяет, закончена ли игра
+    var isGameEnded: Bool { get }    // Начинает новую игру и сразу стартует первый раунд
+    func restartGame()    // Начинает новый раунд (обновляет загаданное число)
+    func startNewRound()    // Сравнивает переданное значение с загаданным и начисляет очки
+    func calculateScore(with value: Int)
+}
+
+class Game: GameProtocol {
+    var score: Int = 0
+    private var minSecretValue: Int
+    private var maxSecretValue: Int
+    var currentSecretValue: Int = 0
+    private var lastRound: Int
+    private var currentRound: Int = 1
+    var isGameEnded: Bool {
+        if currentRound >= lastRound {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init?(startValue: Int, endValue: Int, rounds: Int) {
+        guard startValue <= endValue else {
+            return nil
+        }
+        minSecretValue = startValue
+        maxSecretValue = endValue
+        lastRound = rounds
+        currentSecretValue = self.getNewSecretValue()
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentSecretValue = self.getNewSecretValue()
+        currentRound += 1
+    }
+    
+    private func getNewSecretValue() -> Int {
+        (minSecretValue...maxSecretValue).randomElement()!
+    }
+    
+    func calculateScore(with value: Int) {
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+        } else {
+            score += 50
+        }
+    }
+}
--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,54 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var game: Game!
+    
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
-    var number: Int = 0
-    var round: Int = 1
-    var points: Int = 0
-    let text: Int = 0
-    
-    
-    @IBAction func checkNumber() {    // если игра только начинается
-        // получаем значение на слайдере
-        let numSlider = Int(self.slider.value.rounded())        // сравниваем значение с загаданным        // и подсчитываем очки
-        if numSlider > self.number {
-            self.points += 50 - numSlider + self.number
-        } else if numSlider < self.number {
-            self.points += 50 - self.number + numSlider
-        } else {
-            self.points += 50
-        }
-        if self.round == 5 {            // выводим информационное окно            // с результатами игры
-            let alert = UIAlertController(
-                title: "Игра окончена",
-                message: "Вы заработали \(self.points) очков",
-                preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            self.round = 1
-            self.points = 0
-        } else {
-            self.round += 1
-        }        // генерируем случайное число
-        self.number = Int.random(in: 1...50)  // передаем значение случайного числа в label
-        self.label.text = String(self.number)
-    }
-    
-    
+
     override func loadView() {
         super.loadView()
-        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))    // изменяем текст метки
-        versionLabel.text = "Версия 1.1"    // добавляем метку в родительский view
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        versionLabel.text = "Версия 1.2"
         self.view.addSubview(versionLabel)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.number = Int.random(in: 1...50)
-        self.label.text = String(self.number)        // устанавливаем счетчик раундов на 1
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
+// MARK: - Взаимодействие View - Model
+    @IBAction func checkNumber() {
+        game.calculateScore(with: Int(slider.value))        // Проверяем, окончена ли игра
+        if game.isGameEnded {
+            showAlertWith(score: game.score)            // Начинаем игру заново
+            game.restartGame()
+        } else {
+            game.startNewRound()
+        }        // Обновляем данные о текущем значении загаданного числа
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
+    }
     
+//MARK: - Обновление View        // Обновление текста загаданного числа
+    private func updateLabelWithSecretNumber(newText: String ) {
+        label.text = newText
+    }        // Отображение всплывающего окна со счетом
+    
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(
+            title: "Игра окончена",
+            message: "Вы заработали \(score) очков",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
